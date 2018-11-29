eec2c3bb95aed47953eea6bd99b0cf2707cb213d
lamve2 <venice.lam@oocl.com>
2018-11-29 17:07:51 +0800

    finish

---
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -0,0 +1,27 @@
+package com.tw.apistackbase.api;
+
+import com.tw.apistackbase.core.CompanyRepository;
+import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.core.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyResource {
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @GetMapping(produces = {"application/json"})
+    public Iterable<Company> list() {
+        return companyRepository.findAll();
+    }
+
+    @PostMapping(produces = {"application/json"})
+    public void add(@RequestBody Company company) {
+        for (Employee employee : company.getEmployees()) {
+        employee.setCompany(company);
+        }
+        companyRepository.save(company);
+    }
+}
